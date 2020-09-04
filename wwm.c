42edf3b217fcd9b60e9080428ad2aa4b70b20014
9cg <52229396+9cg@users.noreply.github.com>
2020-09-04 12:08:37 +0300

    Update wwm.c

--- wwm.c
+++ wwm.c
@@ -17,7 +17,6 @@ int main (int argc, char **argv)
     if (xcb_connection_has_error(dpy)) return 1;
 
     screen = xcb_setup_roots_iterator(xcb_get_setup(dpy)).data;
-    root = screen->root;
 
     xcb_grab_key(dpy, 1, screen->root, XCB_MOD_MASK_1, XCB_NO_SYMBOL,
         XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
@@ -50,9 +49,9 @@ int main (int argc, char **argv)
 				values[2] = 3; 
 				xcb_warp_pointer(dpy, XCB_NONE, win, 0, 0, 0, 0, geom->width, geom->height);
 			}
-			xcb_grab_pointer(dpy, 0, root, XCB_EVENT_MASK_BUTTON_RELEASE
-					| XCB_EVENT_MASK_BUTTON_MOTION | XCB_EVENT_MASK_POINTER_MOTION_HINT, 
-					XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, root, XCB_NONE, XCB_CURRENT_TIME);
+   			 xcb_grab_pointer(dpy, 0, screen->root, XCB_EVENT_MASK_BUTTON_RELEASE
+       				 | XCB_EVENT_MASK_BUTTON_MOTION | XCB_EVENT_MASK_POINTER_MOTION_HINT, 
+       				 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, screen->root, XCB_NONE, XCB_CURRENT_TIME);
 			xcb_flush(dpy);
         }
         break;
@@ -60,7 +59,7 @@ int main (int argc, char **argv)
         case XCB_MOTION_NOTIFY:
         {
             xcb_query_pointer_reply_t *pointer;
-            pointer = xcb_query_pointer_reply(dpy, xcb_query_pointer(dpy, root), 0);
+            pointer = xcb_query_pointer_reply(dpy, xcb_query_pointer(dpy, screen->root), 0);
             if (values[2] == 1) {/* move */
                	geom = xcb_get_geometry_reply(dpy, xcb_get_geometry(dpy, win), NULL);
 				values[0] = (pointer->root_x + geom->width > screen->width_in_pixels)?
